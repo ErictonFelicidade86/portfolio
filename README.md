bd1bd962fec7d9b2bb4bae2b112320dbd74f3fe2
Ericton Brito <erictonbrito@outlook.com>
2024-12-31 10:58:14 -0300

    Iniciando o projeto

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# portfolio
+# Meu Portfolio
+
+## Sobre
+
+Analista de Teste/QA especializado em automação de testes com foco em APIs e testes E2E.
+
+<hr>
+
+<div style="display: inline-block">
+  <br>         
+  <img align="center" alt="Ton-HTML5" heigh="50" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />  
+  <img align="center" alt="Ton-CSS3" heigh="50" width="50"  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />               
+  <img align="center" alt="Ton-Javascript" heigh="50" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />               
+  <img align="center" alt="Ton-Git" heigh="50" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />      
+  <img align="center" alt="Ton-Vscode" heigh="50" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" />
+          
+</div>
---
+++ src/js/script.js
@@ -0,0 +1,60 @@
+const navs = document.querySelectorAll('.nav-list li')
+const cube = document.querySelector('.box')
+const sections = document.querySelectorAll('.section')
+
+const resumeLists = document.querySelectorAll('.resume-list')
+const resumeBoxs = document.querySelectorAll('.resume-box')
+
+const portFolioList = document.querySelectorAll('.portfolio-list')
+const portfolioBoxs = document.querySelectorAll('.portfolio-box')
+
+// Navbar actions and all section actions along with cube rotation when navbar is clicked
+navs.forEach((nav, idx) => {
+    nav.addEventListener('click', ()=> {
+        document.querySelector('.nav-list li.active').classList.remove('active')
+        nav.classList.add('active')
+
+        cube.style.transform = `rotateY(${idx * -90}deg)`
+
+        document.querySelector('.section.active').classList.remove('active')
+        sections[idx].classList.add('active')
+
+        const array = Array.from(sections)
+        const arrSecs = array.slice(1, -1)
+
+        arrSecs.forEach(arrSecs => {
+            if (arrSecs.classList.contains('active')) {
+                sections[4].classList.add('action-contact')
+            }   
+        });
+        if (sections[0].classList.contains('active')){
+            sections[4].classList.remove('action-contact');
+        }    
+    })
+})
+
+// Resume section when clicking tab-list
+resumeLists.forEach((list, idx) => {
+    list.addEventListener('click', ()=> {
+        document.querySelector('.resume-list.active').classList.remove('active')
+        list.classList.add('active')
+
+        document.querySelector('.resume-box.active').classList.remove('active')
+        resumeBoxs[idx].classList.add('active')
+    })
+})
+
+// Portfolio section when clicking tab-list
+portFolioList.forEach((list, idx) => {
+    list.addEventListener('click', ()=> {
+        document.querySelector('.portfolio-list.active').classList.remove('active')
+        list.classList.add('active')
+
+        document.querySelector('.portfolio-box.active').classList.remove('active')
+        portfolioBoxs[idx].classList.add('active')
+    })
+})
+
+setTimeout (() => {
+    sections[4].classList.remove('active')
+}, 1500);
